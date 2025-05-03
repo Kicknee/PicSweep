b6302835a3b701cb263c9f2989e613270cb07153
Kicknee <daniszewski21@gmail.com>
2025-05-03 09:15:07 +0200

    add and style main picture

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,35 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
 
 export default function Index() {
+  const PlaceholderImage = require("@/assets/images/background-image.png");
+  const LeftArrowBtn = require("@/assets/images/left-arrow.png");
+  const RightArrowBtn = require("@/assets/images/right-arrow.png");
+  const CancelBtn = require("@/assets/images/cancel.png");
+  const AcceptBtn = require("@/assets/images/accept.png");
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={style.container}>
+      <View style={style.imageContainer}>
+        <Image source={PlaceholderImage} style={style.image} />
+      </View>
     </View>
   );
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#E67263",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
+    width: "80%",
+    position: "relative",
+  },
+  image: {
+    width: "100%",
+    borderRadius: 35,
+    borderColor: "white",
+    borderWidth: 14,
+  },
+});
