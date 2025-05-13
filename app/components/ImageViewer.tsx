5e559abd43f38af7db5c79b980ab67c80e78318f
Kicknee <daniszewski21@gmail.com>
2025-05-14 00:19:01 +0200

    refactor code to fix displaying the same photo on app launch

--- app/components/ImageViewer.tsx
+++ app/components/ImageViewer.tsx
@@ -1,5 +1,12 @@
 import { useState, useEffect, useRef } from "react";
-import { View, Pressable, StyleSheet, Alert, Linking } from "react-native";
+import {
+  Text,
+  View,
+  Pressable,
+  StyleSheet,
+  Alert,
+  Linking,
+} from "react-native";
 import { Image } from "expo-image";
 import * as MediaLibrary from "expo-media-library";
 import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -18,107 +25,102 @@ export default function ImageViewer() {
   const CancelBtn = require("@/assets/images/cancel.png");
   const AcceptBtn = require("@/assets/images/accept.png");
 
-  const [sortedPhotos, setSortedPhotos] = useState<id[]>([]);
+  const [reviewedPhotos, setReviewedPhotos] = useState<id[]>([]);
   const [photos, setPhotos] = useState<Photo[]>([]);
-  const [currentPhotoIndex, setCurrentPhotoIndex] = useState<number>(0);
-
-  const fetchLocalPhotos = async () => {
-    const { status } = await MediaLibrary.requestPermissionsAsync();
-    if (status !== "granted") {
-      Alert.alert(
-        "Camera access required",
-        "You need to manually enable camera access in the app settings.",
-        [
-          { text: "Cancel", style: "cancel" },
-          { text: "Open Settings", onPress: () => Linking.openSettings() },
-        ]
-      );
-
-      return;
-    }
-
-    const media = await MediaLibrary.getAssetsAsync({
-      mediaType: "photo",
-    });
-
-    const fetchedPhotos: Photo[] = media.assets.map((asset) => ({
-      id: asset.id,
-      uri: asset.uri,
-    }));
-    console.log(photos);
-    setPhotos(fetchedPhotos);
-    compareWithSortedPhotos();
-  };
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
-    getIdData();
-    fetchLocalPhotos();
-  }, []);
+    (async () => {
+      //ask for permission to the local library
+      const { status } = await MediaLibrary.requestPermissionsAsync();
+      if (status !== "granted") {
+        Alert.alert(
+          "Camera access required",
+          "You need to manually enable camera access in the app settings.",
+          [
+            { text: "Cancel", style: "cancel" },
+            { text: "Open Settings", onPress: () => Linking.openSettings() },
+          ]
+        );
+        return;
+      }
+      //load reviewed photos from the local storage
+      const saved = await AsyncStorage.getItem("reviewedPhotos");
+      const reviewedList = saved ? JSON.parse(saved) : [];
+      setReviewedPhotos(reviewedList);
 
-  const compareWithSortedPhotos = () => {
-    const checkedPhotos = photos.filter(
-      (photo) => !sortedPhotos.some((p) => p === photo.id)
-    );
-    setPhotos(checkedPhotos);
-  };
+      //load photos from the library
+      const media = await MediaLibrary.getAssetsAsync({
+        mediaType: "photo",
+      });
 
-  const keepPhoto = (id: id) => {
-    setSortedPhotos([...sortedPhotos, id]);
-    storeIdData();
-    showNextPhoto();
-  };
-  const showNextPhoto = () => {
-    if (currentPhotoIndex + 1 < photos.length) {
-      storeIdData();
-      setCurrentPhotoIndex((prev) => prev + 1);
-    } else {
-      Alert.alert("Done", "No more photos to sweep!");
+      //filter photos already reviewed
+      const filtered = media.assets.filter(
+        (asset) => !reviewedPhotos.includes(asset.id)
+      );
+      setPhotos(filtered);
+      setCurrentIndex(0);
+    })();
+  });
+
+  const currentImage = photos[currentIndex];
+
+  const handleKeep = async () => {
+    if (!currentImage) {
+      Alert.alert("Congrats!", "No more photos to sweep!");
+      return;
     }
+    //add ID to the reviewedPhotos state and save in local storage
+    const newList = [...reviewedPhotos, currentImage.id];
+    setReviewedPhotos(newList);
+    await AsyncStorage.setItem("reviewedPhotos", JSON.stringify(newList));
+    //move to the next photo
+    setCurrentIndex((prev) => prev + 1);
   };
 
-  const getIdData = async () => {
-    try {
-      const jsonValue = await AsyncStorage.getItem("sortedPhotos");
-      setSortedPhotos(jsonValue != null ? JSON.parse(jsonValue) : []);
-    } catch (error) {
-      console.error(error);
+  const handleDelete = async () => {
+    if (!currentImage) {
+      Alert.alert("Congrats!", "No more photos to sweep!");
+      return;
     }
+    //delete a photo from the library
+    await MediaLibrary.deleteAssetsAsync(currentImage.id);
+    //add ID to the reviewedPhotos state and save in local storage
+    const newList = [...reviewedPhotos, currentImage.id];
+    setReviewedPhotos(newList);
+    await AsyncStorage.setItem("reviewedPhotos", JSON.stringify(newList));
+    //move to the next photo
+    setCurrentIndex((prev) => prev + 1);
   };
-  const storeIdData = async () => {
-    try {
-      const jsonValue = JSON.stringify(sortedPhotos);
-      await AsyncStorage.setItem("sortedPhotos", jsonValue);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   return (
     <View style={style.imageContainer}>
       <View style={[style.iconsTaskbar, style.iconsTaskbarLeft]}>
         <Image style={[style.icon, style.arrowIcon]} source={LeftArrowBtn} />
-        <Pressable style={style.btn} onPress={showNextPhoto}>
+        <Pressable style={style.btn} onPress={handleDelete}>
           <Image style={[style.icon, style.functionIcon]} source={CancelBtn} />
         </Pressable>
       </View>
-      {photos?.length > 0 ? (
-        <Image
-          source={{ uri: photos[currentPhotoIndex].uri }}
-          style={style.image}
-        />
-      ) : (
-        <Image source={PlaceholderImage} style={style.image} />
-      )}
+      <Image
+        source={currentImage ? { uri: currentImage.uri } : PlaceholderImage}
+        style={style.image}
+      />
 
       <View style={[style.iconsTaskbar, style.iconsTaskbarRight]}>
         <Image style={[style.icon, style.arrowIcon]} source={RightArrowBtn} />
-        <Pressable
-          style={style.btn}
-          onPress={() => keepPhoto(photos[currentPhotoIndex].id)}
-        >
+        <Pressable style={style.btn} onPress={handleKeep}>
           <Image style={[style.icon, style.functionIcon]} source={AcceptBtn} />
         </Pressable>
       </View>
+      {/* <View style={{ position: "fixed", bottom: 0, left: "-10%" }}>
+        <Pressable
+          onPress={() => {
+            console.log("clear");
+            AsyncStorage.clear();
+          }}
+        >
+          <Text>Reset</Text>
+        </Pressable>
+      </View> */}
     </View>
   );
 }
